bc13d9752b030245d37aaf2af23e4ac0424c66c3
bangddong <gusehdqkd@naver.com>
2021-02-22 07:03:09 +0900

    백준 2839 설탕배달

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int bong = 0;
+
+        while(true) {
+            if(n % 5 == 0) {
+                bong += n / 5;
+                System.out.println(bong);
+                break;
+            } else {
+                n -= 3;
+                bong++;
+            }
+
+            if(n < 0) {
+                System.out.println("-1");
+                break;
+            }
+        }
     }
 }
