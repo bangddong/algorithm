5a8f10377ffe4440c045b51ee86d48492e4b98b0
bangddong <gusehdqkd@naver.com>
2021-02-24 07:41:37 +0900

    백준 11399 ATM

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    /**
+     * 사람이 N만큼 주어짐
+     * 각 사람이 주어질 때 인출에 걸리는 시간도 P가 주어짐
+     * 주어진 N을 P의 작은순으로 정렬 후 합
+     */
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 사람 수
+        ArrayList<Integer> p = new ArrayList<>();
 
-        int n = sc.nextInt();
-
-        int bong = 0;
-
-        while(true) {
-            if(n % 5 == 0) {
-                bong += n / 5;
-                System.out.println(bong);
-                break;
-            } else {
-                n -= 3;
-                bong++;
-            }
-
-            if(n < 0) {
-                System.out.println("-1");
-                break;
-            }
+        for (int i = 0; i < n; i++) {
+            p.add(sc.nextInt());
         }
+
+        Collections.sort(p);
+
+        int chainSum = 0;
+        int totSum = 0;
+        for (int num : p) {
+            chainSum += num;
+            totSum += chainSum;
+        }
+
+        System.out.println(totSum);
+
+
     }
 }
