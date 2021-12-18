61687d37d81d0b91a72da2678252b03c134d79f2
bangddong <gusehdqkd@naver.com>
2021-12-18 19:56:16 +0900

    Boj_1240 노드사이의 거리 다익스트라 방법 추가

--- src/Boj/Gold/Boj_1240.java
+++ src/Boj/Gold/Boj_1240.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Boj_1240 {
@@ -12,21 +14,28 @@ public class Boj_1240 {
     static StringTokenizer st;
 
     // 이어지는 정점 좌표와 비용을 담는다.
-    static class Edge {
+    static class Edge implements Comparable<Edge>{
         int y, c;
 
         public Edge(int y, int c) {
             this.y = y;
             this.c = c;
         }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.c - o.c;
+        }
     }
 
     static int N, M;
+    static int[] dist;
     static ArrayList<Edge>[] graph;
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        dist = new int[N + 1];
         graph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
         for (int i = 1; i < N; i++) {
@@ -45,17 +54,38 @@ public class Boj_1240 {
     }
 
     static void solution(int x, int y) {
-        DFS(x, -1, y, 0);
+        //DFS(x, -1, y, 0);
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dijkstra(x, y);
     }
 
-    static void DFS(int x, int prev, int goal, int dist) {
+    static void DFS(int x, int prev, int goal, int cost) {
         if (x == goal) { // 목적지에 도착했다!
-            System.out.println(dist);
+            System.out.println(cost);
             return;
         }
         for (Edge edge : graph[x]) {
             if (edge.y == prev) continue; // 이전 노드는 방문하지 않는다!
-            DFS(edge.y, x, goal, dist + edge.c); // 이동 비용 누적
+            DFS(edge.y, x, goal, cost + edge.c); // 이동 비용 누적
         }
+    }
+
+    static void dijkstra(int x, int y) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(x, 0));
+        dist[x] = 0;
+        while (!pq.isEmpty()) {
+            Edge tmp = pq.poll();
+            int now = tmp.y;
+            int nowCost = tmp.c;
+            if (nowCost > dist[now]) continue;
+            for (Edge edge : graph[now]) {
+                if (dist[edge.y] > nowCost + edge.c) {
+                    dist[edge.y] = nowCost + edge.c;
+                    pq.offer(new Edge(edge.y, nowCost + edge.c));
+                }
+            }
+        }
+        System.out.println(dist[y]);
     }
 }
