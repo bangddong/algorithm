7c42629434f75f778ea6dd9e18fee835f8bb35b9
BangDDong <gusehdqkd@naver.com>
2022-08-12 11:20:10 +0900

    [자료구조] Boj_2743 단어 길이 재기 (구현, 문자열)

---
+++ src/Boj/Bronze/Boj_2743.java
@@ -0,0 +1,16 @@
+package Boj.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_2743 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(br.readLine().length());
+    }
+}
