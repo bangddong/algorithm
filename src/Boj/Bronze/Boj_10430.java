a096a06dd5db167e4223557511deb076e4e45507
bangddong <gusehdqkd@naver.com>
2022-08-16 12:39:34 +0900

    [수학] Boj_10430 나머지 (수학, 구현, 사칙연산)

---
+++ src/Boj/Bronze/Boj_10430.java
@@ -0,0 +1,24 @@
+package Boj.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_10430 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c) ;
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
