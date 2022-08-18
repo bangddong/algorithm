f31d98ec31b80526b01fea34547a2a0bddf858ff
BangDDong <gusehdqkd@naver.com>
2022-08-18 23:30:45 +0900

    [자료구조] Boj_1929 소수 구하기 (수학, 정수론, 소수 판정, 에라토스테네스의 체)

--- src/Boj/Silver/Boj_1978.kt
+++ src/Boj/Silver/Boj_1978.kt
@@ -2,29 +2,31 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
 
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    var n = br.readLine().toInt()
-    val st = StringTokenizer(br.readLine())
+class Boj_1978 {
+    fun main() {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        var n = br.readLine().toInt()
+        val st = StringTokenizer(br.readLine())
 
-    var tot = 0
-    while (n --> 0) {
-        if (isPrime(st.nextToken().toInt()))
-            tot++
+        var tot = 0
+        while (n --> 0) {
+            if (isPrime(st.nextToken().toInt()))
+                tot++
 
+        }
+
+        println(tot)
     }
 
-    println(tot)
-}
+    fun isPrime(num: Int): Boolean {
+        if (num == 1) return false
 
-fun isPrime(num: Int): Boolean {
-    if (num == 1) return false
+        var i = 2
+        while (i * i <= num) {
+            if (num % i == 0) return false
+            i++
+        }
 
-    var i = 2
-    while (i * i <= num) {
-        if (num % i == 0) return false
-        i++
+        return true
     }
-
-    return true
 }
